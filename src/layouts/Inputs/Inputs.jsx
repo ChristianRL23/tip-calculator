95741cc38520abdc74beb233b06d720ff1a74aab
Christian Lara <christianramirezlara23@gmail.com>
2022-01-25 10:35:47 -0600

    Positionate two Inputs components

--- src/layouts/Inputs/Inputs.jsx
+++ src/layouts/Inputs/Inputs.jsx
@@ -1,11 +1,16 @@
 import './Inputs.scss';
 import dollarIcon from './icon-dollar.svg';
+import personIcon from './icon-person.svg';
 import InputsContainer from '../../components/InputsContainer/InputsContainer';
 
 const Inputs = () => {
   return (
     <div className="inputs">
       <InputsContainer inputDescription="Bill" inputIcon={dollarIcon} />
+      <InputsContainer
+        inputDescription="Number of People"
+        inputIcon={personIcon}
+      />
     </div>
   );
 };
