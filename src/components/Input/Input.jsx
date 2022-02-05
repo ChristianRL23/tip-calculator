7607eb34a4df57b33f58c5cb2c9fbf3f2b7e8232
Christian Lara <christianramirezlara23@gmail.com>
2022-02-05 07:16:25 -0600

    Add focus and blur styles to the Input component

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,10 +1,31 @@
 import './Input.scss';
+import { useState } from 'react';
 
 const Input = ({ inputIcon }) => {
+  const [inputFocus, setInputFocus] = useState(false);
+
+  const focusInputFn = () => setInputFocus(true);
+
+  const blurInputFn = () => setInputFocus(false);
+
+  let inputStateClassName;
+
+  if (!inputFocus) {
+    inputStateClassName = 'input';
+  } else if (inputFocus) {
+    inputStateClassName = 'input--focus';
+  }
+
   return (
-    <div className="input">
+    <div className={inputStateClassName}>
       <img className="input__icon" src={inputIcon} alt="Input icon" />
-      <input placeholder="0" type="number" className="input__text-area" />
+      <input
+        onBlur={blurInputFn}
+        onFocus={focusInputFn}
+        placeholder="0"
+        type="number"
+        className="input__text-area"
+      />
     </div>
   );
 };
