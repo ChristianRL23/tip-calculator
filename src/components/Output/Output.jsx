28e4f75bf5ddcef3f6827e4f1348b9d96455b8be
Christian Lara <christianramirezlara23@gmail.com>
2022-02-06 16:15:34 -0600

    Calculate the tip

--- src/components/Output/Output.jsx
+++ src/components/Output/Output.jsx
@@ -1,13 +1,15 @@
 import './Output.scss';
 
-const Output = ({ description }) => {
+const Output = ({ description, tip, secondaryDescription }) => {
   return (
     <div className="output">
       <div className="output__description">
         <h5 className="output__description__main">{description}</h5>
-        <h6 className="output__description__secondary">/ persona</h6>
+        <h6 className="output__description__secondary">
+          {secondaryDescription}
+        </h6>
       </div>
-      <h2 className="output__value">$00.00</h2>
+      <h2 className="output__value">{`$${tip.toFixed(2)}`}</h2>
     </div>
   );
 };
--- src/layouts/Outputs/Outputs.jsx
+++ src/layouts/Outputs/Outputs.jsx
@@ -2,36 +2,67 @@
 import Output from '../../components/Output/Output';
 import './Outputs.scss';
 import Button from './../../components/Button/Button';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import TipContext from './../../context/TipContext';
 
 const Outputs = () => {
+  const [totalTip, setTotalTip] = useState({
+    totalTip: 0,
+    tipPerPerson: 0,
+  });
+
+  let inputsValid;
   const tipCtx = useContext(TipContext);
   const calculateTip = () => {
     if (tipCtx.billTotal === '') {
       tipCtx.setBillInputError('No puede estar vacío');
+      inputsValid = false;
     } else if (tipCtx.billTotal <= 0) {
       tipCtx.setBillInputError('Debe ser mayor a cero');
+      inputsValid = false;
     }
     if (tipCtx.tipPercentage === '') {
       tipCtx.setTipInputError('No puede estar vacío');
+      inputsValid = false;
     } else if (tipCtx.tipPercentage <= 0) {
       tipCtx.setTipInputError('Debe ser mayor a cero');
+      inputsValid = false;
     } else if (tipCtx.tipPercentage > 100) {
       tipCtx.setTipInputError('Debe ser menor a cien');
+      inputsValid = false;
     }
     if (tipCtx.persons === '') {
       tipCtx.setPersonsInputError('No puede estar vacío');
+      inputsValid = false;
     } else if (tipCtx.persons <= 0) {
       tipCtx.setPersonsInputError('Debe ser mayor a cero');
+      inputsValid = false;
+    }
+
+    if (inputsValid === undefined) {
+      const totalTip =
+        (Number(tipCtx.tipPercentage) * Number(tipCtx.billTotal)) / 100;
+      const tipPerPerson = totalTip / Number(tipCtx.persons);
+      setTotalTip({
+        totalTip,
+        tipPerPerson,
+      });
     }
   };
 
   return (
     <div className="outputs">
       <div className="outputs__outputs">
-        <Output description="Propina" />
-        <Output description="Total" />
+        <Output
+          tip={totalTip.tipPerPerson}
+          secondaryDescription="por persona"
+          description="Propina"
+        />
+        <Output
+          tip={totalTip.totalTip}
+          secondaryDescription="de propina"
+          description="Total"
+        />
       </div>
       <div className="outputs__buttons">
         <Button clickFn={calculateTip} style="strong" content="CALCULAR" />
